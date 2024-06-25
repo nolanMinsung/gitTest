92431bd529a743b3cd5d2fb96d7b51447287db74
Minsung Kim <mskim4048@naver.com>
2024-06-25 16:58:01 +0900

    Initial Commit

---
+++ GitTestUITests/GitTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GitTestUITestsLaunchTests.swift
+//  GitTestUITests
+//
+//  Created by 김민성 on 2024/06/25.
+//
+
+import XCTest
+
+final class GitTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
